75c1dc1c7f0ec1b3ecbb8dd394d6c6e9b898a714
Maciek Bulski <fragmast3r@gmail.com>
2019-03-11 00:42:41 +0000

    first commit for new version

---
+++ js/script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+console.log("document is ready");
+  
+  
+  jQuery('.mdl[href^=#]').click(function(e){
+    e.preventDefault();
+    var href = jQuery(this).attr('href');
+    jQuery(href).modal('toggle');
+  });
+
+  
+
+});  
